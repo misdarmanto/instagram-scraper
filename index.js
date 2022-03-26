c2df17ac03092fe9f89eeb8a37352af3f91171ab
misdarmanto <misdarmanto1922@gmail.com>
2022-03-26 15:17:13 +0700

    change code

--- index.js
+++ index.js
@@ -2,8 +2,6 @@ var express = require("express");
 let app = express();
 let bodyParser = require("body-parser");
 const instagram = require("@phaticusthiccy/open-apis");
-const tiktokscraper = require("tiktok-scraper-ts");
-const { TTScraper } = require("tiktok-scraper-ts");
 
 const port = process.env.PORT || 3000;
 
@@ -33,22 +31,6 @@ app.post("/instagram/download", (req, res) => {
     res.status(200).json(data);
   }
   start();
-});
-
-app.post("/instagram/story", (req, res) => {
-  async function start() {
-    var data = await instagram.insta_story(req.body.userName);
-    res.status(200).json(data);
-  }
-  start();
-});
-
-app.post("/instagram/tiktok", (req, res) => {
-  const TikTokScraper = new TTScraper();
-  (async () => {
-    const fetchVideo = await TikTokScraper.video(req.body.url);
-    res.status(200).json(fetchVideo);
-  })();
 });
 
 app.listen(port, () => {
