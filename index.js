58104cb9cbd043ef630abcd3840b65f556fe6796
misdarmanto <misdarmanto1922@gmail.com>
2022-03-26 14:59:21 +0700

    add new functionality

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ var express = require("express");
 let app = express();
 let bodyParser = require("body-parser");
 const instagram = require("@phaticusthiccy/open-apis");
+const tiktokscraper = require("tiktok-scraper-ts");
+const { TTScraper } = require("tiktok-scraper-ts");
 
 const port = process.env.PORT || 3000;
 
@@ -10,7 +12,7 @@ app.use(
   bodyParser.urlencoded({
     extended: true,
   })
-); 
+);
 
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
@@ -30,7 +32,23 @@ app.post("/instagram/download", (req, res) => {
     var data = await instagram.insta_post(req.body.url);
     res.status(200).json(data);
   }
-  start()
+  start();
+});
+
+app.post("/instagram/story", (req, res) => {
+  async function start() {
+    var data = await instagram.insta_story(req.body.userName);
+    res.status(200).json(data);
+  }
+  start();
+});
+
+app.post("/instagram/tiktok", (req, res) => {
+  const TikTokScraper = new TTScraper();
+  (async () => {
+    const fetchVideo = await TikTokScraper.video(req.body.url);
+    res.status(200).json(fetchVideo);
+  })();
 });
 
 app.listen(port, () => {
